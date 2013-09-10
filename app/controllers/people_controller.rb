c6317a678f5360c303756e6f4d6b5648d2a418e4
Kevin VanderLugt <kevin.vanderlugt@gmail.com>
2013-09-10 13:12:48 -0700

    Adds ability to create a new person but no nested attributes

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -9,9 +9,31 @@ class PeopleController < ApplicationController
   end
 
   def new
+    @person = Person.new
   end
 
   def edit
+  end
+
+  def create
+    person = Person.new(person_params)
+    if person.save
+      flash[:success] = "Person created"
+      redirect_to people_url
+    else
+      flash[:error] = "Unable to create person"
+      render 'new'
+    end
+  end
+
+  def update
+    if @person.update(person_params)
+      flash[:success] = "Person updated"
+      redirect_to people_url
+    else
+      flash[:error] = "Unable to update person"
+      render 'edit'
+    end
   end
 
   def destroy
@@ -26,5 +48,9 @@ class PeopleController < ApplicationController
   private
     def find_person
       @person = Person.find(params[:id])
+    end
+
+    def person_params
+      params.require(:person).permit(:first_name, :last_name)
     end
 end
